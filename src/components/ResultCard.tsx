e534801ba54607f008821f7544fecdb093f7ec72
wnmay <wanathachat@gmail.com>
2025-02-09 14:47:01 +0700

    fix: edit card result flip and test

--- src/components/ResultCard.tsx
+++ src/components/ResultCard.tsx
@@ -7,33 +7,40 @@ export default function ResultCard({ imageId }: { imageId: string }) {
   return (
     <div className="flex justify-center items-center h-screen">
       <motion.div
-        className="relative w-60 h-110 cursor-pointer"
+        className="relative w-70 h-130 cursor-pointer"
         onClick={() => setIsFlipped(!isFlipped)}
         initial={false}
         animate={{ rotateY: isFlipped ? 180 : 0 }}
         transition={{ duration: 1 }}
+        style={{
+          transformStyle: "preserve-3d",
+        }}
       >
         <motion.div
           className="absolute w-full h-full flex items-center justify-center"
-          animate={{ rotateY: isFlipped ? 0 : -180 }}
-          transition={{ duration: 1 }}
+          style={{
+            backfaceVisibility: "hidden",
+          }}
         >
           <img
             src="/img/card/card-back.webp"
             alt="Card Back"
-            className="block w-full h-full object-cover"
+            className="block w-full h-full object-cover rounded-lg shadow-lg"
           />
         </motion.div>
 
         <motion.div
-          className="absolute w-full h-full items-center justify-center"
-          animate={{ rotateY: isFlipped ? 180 : 0 }}
-          transition={{ duration: 1 }}
+          className="absolute w-full h-full flex items-center justify-center"
+          animate={{ rotateY: 180 }}
+          style={{
+            backfaceVisibility: "hidden",
+            transform: "rotateY(180deg)",
+          }}
         >
           <img
-            src="/img/card/card-back.webp"
-            alt="Card Back"
-            className="block w-full h-full object-cover"
+            src={`/img/card/result/${imageId}.webp`}
+            alt="Card Front"
+            className="block w-full h-full object-cover rounded-lg shadow-lg"
           />
         </motion.div>
       </motion.div>
