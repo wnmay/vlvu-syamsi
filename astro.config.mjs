77604c7a4b376b4e670f07239fa0466116e782ff
wnmay <wanathachat@gmail.com>
2025-02-09 16:36:16 +0700

    chore: add vercel adapter for deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,12 @@ import { defineConfig } from "astro/config";
 
 import react from "@astrojs/react";
 import tailwindcss from "@tailwindcss/vite";
+import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
+  adapter: vercel(),
   integrations: [react()],
 
   vite: {
